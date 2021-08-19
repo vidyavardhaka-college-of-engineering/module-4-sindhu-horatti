c8d77e7de71473356caec1a2f46f53bace2f5f93
sindhu-horatti <84697125+sindhu-horatti@users.noreply.github.com>
2021-08-19 12:52:37 +0530

    Done

--- Program-5.c
+++ Program-5.c
@@ -1,3 +1,4 @@
+
 // Program to retrieve book details
 //Input: Pro-c 200 20 
 //Output:Pro-c 200 20
@@ -6,14 +7,17 @@
 int main() 
 { 
     struct book 
-    { 
-       //declare name and price
-        int pages; 
+    {  
+        char name[50]; 
+        int price; //declare name and price
+        int pages;
+       
     }; 
     struct book b1;
      
     printf("Enter names, prices & no. of pages of 3 books"); 
-    scanf("%s%d%d", &b1.name, &b1.price, &b1.pages);  
+    scanf("%s%d%d", b1.name, &b1.price, &b1.pages);  
     printf("%s %d %d", b1.name, b1.price, b1.pages); 
      return 0;
 } 
+//DONE
